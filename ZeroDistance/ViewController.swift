6238464cfca5425c21b0664e4c5f08b74cfe5c5d
crise1990 <crise1990@qq.com>
2016-05-22 22:24:51 +0800

    Initial Commit

---
+++ ZeroDistance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZeroDistance
+//
+//  Created by mac on 16/5/22.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
